e437bbaefacca33efc32de2225a5e98d914d869f
shlee <realnavystyle@gmail.com>
2019-07-30 17:38:57 +0900

    .

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production: false,
+    env: 'dev',
+    auth0: {
+        domain: 'realnavystyle.auth0.com',
+        clientId: '6wZ6KBbDXxMeYAOJZFOuNEt24gHwBfQ1',
+        callbackURL: 'http://localhost:4200/callback'
+    },
+    apiUrl: 'http://localhost:3000'
+};
